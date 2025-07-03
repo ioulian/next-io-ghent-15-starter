5f7b1037dc40d83d93f2237a5517c79fdfddd924
Yulian Alexeyev <yulian.alexeyev+github@gmail.com>
2025-07-03 11:15:32 +0200

    add promise race

--- src/utils/promises.ts
+++ src/utils/promises.ts
@@ -1,5 +1,21 @@
 /**
  * Will stall promise for a specific time
+ *
  * @param ms Number of miliseconds
  */
 export const wait = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+/**
+ * Will race a promise with a timeout.
+ * From: https://playfulprogramming.com/posts/nextjs-promise-race
+ *
+ * @param promise Promise to race
+ * @param timeout Timeout in milliseconds
+ * @returns Promise that will resolve with the result of the promise or reject with the timeout
+ */
+export const race = (promise: Promise<unknown>, timeout: number = 1000) => {
+  return Promise.any([
+    promise,
+    new Promise<void>((resolve) => setTimeout(() => resolve(), timeout)),
+  ]);
+};
