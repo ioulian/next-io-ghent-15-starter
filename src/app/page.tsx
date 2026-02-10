d06f060bb7337b84bfd4e519e33e2b8e41b031f2
Yulian Alexeyev <yulian.alexeyev+github@gmail.com>
2026-02-10 13:05:25 +0100

    minor typing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 import { defaultLocale } from "@/i18n/constants";
 
 // This page only renders when the app is built statically (output: 'export')
-const Page: NextPage = ({}) => {
+const Page: NextPage<PageProps<"/">> = () => {
   redirect(`/${defaultLocale}`);
 };
 
